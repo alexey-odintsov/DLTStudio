7c8bc2fec580a853338b5bb88e3e2a10992df8ef
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2024-01-27 10:59:36 +0100

    StatusBar layout updated

--- composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/ui/MainWindow.kt
+++ composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/ui/MainWindow.kt
@@ -4,7 +4,10 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Text
@@ -14,8 +17,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -44,19 +49,42 @@ fun MainWindow() {
         }
 
         LazyScrollable(modifier = Modifier.weight(1f).background(Color.LightGray), dltSession)
-        StatusBar(modifier = Modifier.fillMaxWidth(), progress)
+        StatusBar(modifier = Modifier.fillMaxWidth(), progress, dltSession)
+    }
+}
+
+
+@Composable
+fun StatusBar(modifier: Modifier = Modifier, progress: Float, dltSession: ParseSession?) {
+    Row(modifier = modifier.height(30.dp).padding(4.dp)) {
+        if (dltSession != null) {
+            Text(text = "File: ${dltSession.file.absoluteFile} Messages: ${"%,d".format(dltSession.dltMessages.size)}")
+        } else {
+            Text(text = "No file loaded")
+        }
+        if (progress > 0f) {
+            LinearProgressIndicator(
+                modifier = Modifier.weight(1f).padding(start = 4.dp, end = 4.dp)
+                    .align(Alignment.CenterVertically),
+                backgroundColor = Color.White,
+                progress = progress,
+            )
+        }
     }
 }
 
 @Preview
 @Composable
-fun StatusBar(modifier: Modifier = Modifier, progress: Float) {
-    LinearProgressIndicator(
-        modifier = Modifier.fillMaxWidth(),
-        backgroundColor = Color.White,
-        progress = progress,
+fun PreviewStatusBarNoSession() {
+    StatusBar(Modifier.background(Color.LightGray), 0f, null)
+}
+
+@Preview
+@Composable
+fun PreviewStatusBarInProgress() {
+    StatusBar(
+        Modifier.background(Color.LightGray),
+        0.4f,
+        ParseSession({ i -> }, File("/user/test/file.dlt"))
     )
-    Box(modifier = modifier) {
-        Text(text = "Status")
-    }
 }
