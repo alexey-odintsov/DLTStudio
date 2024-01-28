07a1c42eaf8ee0f0974307b1dfd232cebab1d6a4
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2024-01-28 10:02:59 +0100

    Make Log info selectable

--- composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/ui/LogPreview.kt
+++ composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/ui/LogPreview.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
@@ -27,73 +28,103 @@ import com.alekso.dltparser.toHex
 @Composable
 fun LogPreview(modifier: Modifier, dltMessage: DLTMessage?) {
     val paddingModifier = Modifier.padding(start = 4.dp, end = 4.dp)
-    Divider()
-    Column(modifier = modifier.then(Modifier.verticalScroll(rememberScrollState()))) {
-        if (dltMessage != null) {
-            Text(
-                modifier = paddingModifier,
-                fontFamily = FontFamily.Monospace,
-                fontWeight = FontWeight(600),
-                fontSize = 11.sp,
-                text = "Standard header:"
-            )
-            MonoText(
-                modifier = paddingModifier,
-                text = "Header Type: ${dltMessage.standardHeader.headerType.originalByte.toHex()} (${
-                    dltMessage.standardHeader.headerType.originalByte.toString(
-                        2
-                    ).padStart(8, '0')
-                })"
-            )
-            MonoText(
-                modifier = paddingModifier,
-                text = "${dltMessage.standardHeader.headerType}"
-            )
-            MonoText(
-                modifier = paddingModifier,
-                text = "${dltMessage.standardHeader}"
-            )
-            Divider()
-            Text(
-                modifier = paddingModifier,
-                fontFamily = FontFamily.Monospace,
-                fontWeight = FontWeight(600),
-                fontSize = 11.sp,
-                text = "Extended header:"
-            )
-            MonoText(
-                modifier = paddingModifier,
-                text = "${dltMessage.extendedHeader}"
-            )
-            Divider()
-            Text(
-                modifier = paddingModifier,
-                fontFamily = FontFamily.Monospace,
-                fontWeight = FontWeight(600),
-                fontSize = 11.sp,
-                text = "Payload:"
-            )
-            if (dltMessage.payload != null) {
-                val payload = dltMessage.payload as VerbosePayload
+    SelectionContainer {
+
+        Divider()
+        Column(modifier = modifier.then(Modifier.verticalScroll(rememberScrollState()))) {
+            if (dltMessage != null) {
                 Text(
                     modifier = paddingModifier,
                     fontFamily = FontFamily.Monospace,
+                    fontWeight = FontWeight(600),
                     fontSize = 11.sp,
-                    text = "Arguments found: ${payload.arguments.size}"
+                    text = "Standard header:"
                 )
+                MonoText(
+                    modifier = paddingModifier,
+                    text = "Header Type: ${dltMessage.standardHeader.headerType.originalByte.toHex()} (${
+                        dltMessage.standardHeader.headerType.originalByte.toString(
+                            2
+                        ).padStart(8, '0')
+                    })"
+                )
+                MonoText(
+                    modifier = paddingModifier,
+                    text = "${dltMessage.standardHeader.headerType}"
+                )
+                MonoText(
+                    modifier = paddingModifier,
+                    text = "${dltMessage.standardHeader}"
+                )
+                Divider()
+                Text(
+                    modifier = paddingModifier,
+                    fontFamily = FontFamily.Monospace,
+                    fontWeight = FontWeight(600),
+                    fontSize = 11.sp,
+                    text = "Extended header:"
+                )
+                MonoText(
+                    modifier = paddingModifier,
+                    text = "${dltMessage.extendedHeader}"
+                )
+                Divider()
+                Text(
+                    modifier = paddingModifier,
+                    fontFamily = FontFamily.Monospace,
+                    fontWeight = FontWeight(600),
+                    fontSize = 11.sp,
+                    text = "Payload:"
+                )
+                if (dltMessage.payload != null) {
+                    val payload = dltMessage.payload as VerbosePayload
+                    Text(
+                        modifier = paddingModifier,
+                        fontFamily = FontFamily.Monospace,
+                        fontSize = 11.sp,
+                        text = "Arguments found: ${payload.arguments.size}"
+                    )
 
-                Row(modifier = paddingModifier) {
-                    MonoText(modifier = Modifier.width(20.dp).padding(end = 1.dp).background(color = Color(250, 250, 250)), text = "#")
-                    MonoText(modifier = Modifier.width(120.dp).padding(end = 1.dp).background(color = Color(250, 250, 250)), text = "type")
-                    MonoText(modifier = Modifier.width(40.dp).padding(end = 1.dp).background(color = Color(250, 250, 250)), text = "size")
-                    MonoText(modifier = Modifier.weight(1f).padding(end = 1.dp).background(color = Color(250, 250, 250)), text = "payload")
-                }
-                payload.arguments.forEachIndexed { index, it ->
                     Row(modifier = paddingModifier) {
-                        MonoText(modifier = Modifier.width(20.dp).padding(end = 1.dp).background(color = Color(250, 250, 250)), text = "$index")
-                        MonoText(modifier = Modifier.width(120.dp).padding(end = 1.dp).background(color = Color(250, 250, 250)), text = it.typeInfo.getTypeString())
-                        MonoText(modifier = Modifier.width(40.dp).padding(end = 1.dp).background(color = Color(250, 250, 250)), text = "${it.payloadSize}")
-                        MonoText(modifier = Modifier.weight(1f).padding(end = 1.dp).background(color = Color(250, 250, 250)), text = it.getPayloadAsText())
+                        MonoText(
+                            modifier = Modifier.width(20.dp).padding(end = 1.dp)
+                                .background(color = Color(250, 250, 250)), text = "#"
+                        )
+                        MonoText(
+                            modifier = Modifier.width(120.dp).padding(end = 1.dp)
+                                .background(color = Color(250, 250, 250)), text = "type"
+                        )
+                        MonoText(
+                            modifier = Modifier.width(40.dp).padding(end = 1.dp)
+                                .background(color = Color(250, 250, 250)), text = "size"
+                        )
+                        MonoText(
+                            modifier = Modifier.weight(1f).padding(end = 1.dp)
+                                .background(color = Color(250, 250, 250)), text = "payload"
+                        )
+                    }
+                    payload.arguments.forEachIndexed { index, it ->
+                        Row(modifier = paddingModifier) {
+                            MonoText(
+                                modifier = Modifier.width(20.dp).padding(end = 1.dp)
+                                    .background(color = Color(250, 250, 250)), text = "$index"
+                            )
+                            MonoText(
+                                modifier = Modifier.width(120.dp).padding(end = 1.dp)
+                                    .background(color = Color(250, 250, 250)),
+                                text = it.typeInfo.getTypeString()
+                            )
+                            MonoText(
+                                modifier = Modifier.width(40.dp).padding(end = 1.dp)
+                                    .background(color = Color(250, 250, 250)),
+                                text = "${it.payloadSize}"
+                            )
+                            MonoText(
+                                modifier = Modifier.weight(1f).padding(end = 1.dp)
+                                    .background(color = Color(250, 250, 250)),
+                                text = it.getPayloadAsText()
+                            )
+                        }
                     }
                 }
             }
