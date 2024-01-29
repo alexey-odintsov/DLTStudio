4d6119dd8604179757d6aa47a9144b0638c8f340
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2024-01-30 00:49:44 +0100

    Data types rework

--- dltparser/src/commonMain/kotlin/com/alekso/dltparser/DataUtils.kt
+++ dltparser/src/commonMain/kotlin/com/alekso/dltparser/DataUtils.kt
@@ -1,19 +1,101 @@
 package com.alekso.dltparser
 
+
+private const val DEBUG_LOG = false
+
 enum class Endian {
-    BIG,
-    LITTLE
+    BIG, LITTLE
 }
 
+// region isBitSet
 fun Byte.isBitSet(position: Int): Boolean {
-    return ((toInt() shl (position.inv() and 31)) < 0)
+    val mask = 1 shl position
+    val result = this.toInt() and mask
+    if (DEBUG_LOG) {
+        println("this:   " + toBinary(8))
+        println("mask:   " + mask.toBinary(8))
+        println("masked: " + result.toBinary(8))
+    }
+    return (this.toInt() and result) == mask
+}
+
+fun Short.isBitSet(position: Int): Boolean {
+    val mask = 1 shl position
+    val result = this.toInt() and mask
+    if (DEBUG_LOG) {
+        println("this:   " + toBinary(16))
+        println("mask:   " + mask.toBinary(16))
+        println("masked: " + result.toBinary(16))
+    }
+    return (this.toInt() and result) == mask
 }
 
 fun Int.isBitSet(position: Int): Boolean {
-    return ((this shl (position.inv() and 31)) < 0)
+    val mask = 1 shl position
+    val result = this and mask
+    if (DEBUG_LOG) {
+        println("this:   " + toBinary(32))
+        println("mask:   " + mask.toBinary(32))
+        println("masked: " + result.toBinary(32))
+    }
+    return (this and result) == mask
+}
+
+/**
+ * Checks if bit at position is set (LSB order)
+ */
+fun Long.isBitSet(position: Int): Boolean {
+    val mask = 1L shl position
+    val result = this and mask
+    if (DEBUG_LOG) {
+        println("this:   " + toBinary(64))
+        println("mask:   " + mask.toBinary(64))
+        println("masked: " + result.toBinary(64))
+    }
+    return (this and result) == mask
+}
+// endregion
+
+// region toBinary
+fun Int.toBinary(bits: Int): String {
+    return toUInt().toString(2).padStart(bits, '0')
+}
+
+fun Byte.toBinary(bits: Int): String {
+    return toUByte().toString(2).padStart(bits, '0')
+}
+
+fun Short.toBinary(bits: Int): String {
+    return toUShort().toString(2).padStart(bits, '0')
+}
+
+fun Long.toBinary(bits: Int): String {
+    return toULong().toString(2).padStart(bits, '0')
+}
+// endregion
+
+// region toHex
+fun Int.toHex(bytes: Int): String {
+    return toUInt().toString(16).padStart(bytes * 2, '0')
+}
+
+fun Short.toHex(bytes: Int): String {
+    return toUShort().toString(16).padStart(bytes * 2, '0')
 }
 
 fun Byte.toHex(): String = "%02x".format(this)
+
+fun Long.toHex(bytes: Int): String {
+    return toULong().toString(16).padStart(bytes * 2, '0')
+}
+
+
+fun ByteArray.toHex(): String =
+    joinToString(separator = " ", limit = 256) { eachByte -> "%02x".format(eachByte) }
+// endregion
+
+
+// region ByteArray reading
 
 fun ByteArray.readString(start: Int, bytes: Int) = decodeToString(start, start + bytes)
 
@@ -44,17 +126,13 @@ fun ByteArray.readUShort(start: Int, endian: Endian): UShort {
  */
 fun ByteArray.readInt(start: Int, endian: Endian): Int {
     return if (endian == Endian.LITTLE) {
-        (get(start + 3).toInt() shl 24) or
-                (get(start + 2).toInt() and 0xff shl 16) or
-                (get(start + 1).toInt() and 0xff shl 8) or
-                (get(start + 0).toInt() and 0xff)
+        (get(start + 3).toInt() shl 24) or (get(start + 2).toInt() and 0xff shl 16) or (get(start + 1).toInt() and 0xff shl 8) or (get(
+            start + 0
+        ).toInt() and 0xff)
     } else {
-        (get(start + 0).toInt() shl 24) or
-                (get(start + 1).toInt() and 0xff shl 16) or
-                (get(start + 2).toInt() and 0xff shl 8) or
-                (get(start + 3).toInt() and 0xff)
+        (get(start + 0).toInt() shl 24) or (get(start + 1).toInt() and 0xff shl 16) or (get(start + 2).toInt() and 0xff shl 8) or (get(
+            start + 3
+        ).toInt() and 0xff)
     }
 }
-
-fun ByteArray.toHex(): String =
-    joinToString(separator = " ", limit = 256) { eachByte -> "%02x".format(eachByte) }
+// endregion
---
+++ dltparser/src/commonTest/kotlin/BitwiseOperationsTests.kt
@@ -0,0 +1,126 @@
+import com.alekso.dltparser.isBitSet
+import org.junit.Assert
+import org.junit.Test
+
+class BitwiseOperationsTests {
+
+    @Test
+    fun `Int isBitSet test 1`() {
+        val data = 0b10000000_00000000_00000000_00000000.toInt()
+        val actual = data.isBitSet(0)
+        Assert.assertTrue(!actual)
+    }
+
+    @Test
+    fun `Int isBitSet test 2`() {
+        val data = 0b00000001000000001000000010000000
+        val actual = data.isBitSet(7)
+        Assert.assertTrue(actual)
+    }
+
+    @Test
+    fun `Int isBitSet test 3`() {
+        val data = 0b10000001000000001000000010000001.toInt()
+        val actual = data.isBitSet(31)
+        Assert.assertTrue(actual)
+    }
+
+    @Test
+    fun `Int isBitSet test 4`() {
+        val data = 0b10000001000000001000000010000100.toInt()
+        val actual = data.isBitSet(3)
+        Assert.assertTrue(!actual)
+    }
+
+    @Test
+    fun `Int isBitSet test 5`() {
+        val data = 0b00000001000000001000000010000000
+        val actual = data.isBitSet(31)
+        Assert.assertTrue(!actual)
+    }
+
+    @Test
+    fun `Byte isBitSet test 1`() {
+        val data = 0b00000001.toByte()
+        val actual = data.isBitSet(0)
+        Assert.assertTrue(actual)
+    }
+
+    @Test
+    fun `Byte isBitSet test 2`() {
+        val data = 0b00010001.toByte()
+        val actual = data.isBitSet(4)
+        Assert.assertTrue(actual)
+    }
+
+    @Test
+    fun `Byte isBitSet test 3`() {
+        val data = 0b10010001.toByte()
+        val actual = data.isBitSet(1)
+        Assert.assertTrue(!actual)
+    }
+
+    @Test
+    fun `Byte isBitSet test 4`() {
+        val data = 0b00010000.toByte()
+        val actual = data.isBitSet(0)
+        Assert.assertTrue(!actual)
+    }
+
+    @Test
+    fun `Short isBitSet test 1`() {
+        val data = 0b00010001_01100011.toShort()
+        val actual = data.isBitSet(0)
+        Assert.assertTrue(actual)
+    }
+
+    @Test
+    fun `Short isBitSet test 2`() {
+        val data = 0b00010001_01100011.toShort()
+        val actual = data.isBitSet(12)
+        Assert.assertTrue(actual)
+    }
+
+    @Test
+    fun `Short isBitSet test 3`() {
+        val data = 0b10010001_01100011.toShort()
+        val actual = data.isBitSet(15)
+        Assert.assertTrue(actual)
+    }
+
+    @Test
+    fun `Short isBitSet test 4`() {
+        val data = 0b10010001_01100011.toShort()
+        val actual = data.isBitSet(0)
+        Assert.assertTrue(actual)
+    }
+
+    @Test
+    fun `Long isBitSet test 1`() {
+        val data = 0b00000001000000001000000010000000_00000001000000001000000010000000
+        val actual = data.isBitSet(0)
+        Assert.assertTrue(!actual)
+    }
+
+    @Test
+    fun `Long isBitSet test 2`() {
+        val data = 0b00000001000000001000000010000000_00000001000000001000000010000000
+        val actual = data.isBitSet(63)
+        Assert.assertTrue(!actual)
+    }
+
+    @Test
+    fun `Long isBitSet test 3`() {
+        val data = 0x4000000000000000 shl 1
+        val actual = data.isBitSet(63)
+        Assert.assertTrue(actual)
+    }
+
+    @Test
+    fun `Long isBitSet test 4`() {
+        val data = 3L
+        val actual = data.isBitSet(1)
+        Assert.assertTrue(actual)
+    }
+
+}
