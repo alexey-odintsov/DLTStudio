384adf95d32e4e6c51f0070f3a950589e90b4adb
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2024-01-31 23:29:04 +0100

    Timeline list added

--- composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/ui/timeline/TimeLinePanel.kt
+++ composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/ui/timeline/TimeLinePanel.kt
@@ -1,8 +1,17 @@
 package com.alekso.dltstudio.ui.timeline
 
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -12,11 +21,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.alekso.dltstudio.ui.ParseSession
 import com.alekso.dltstudio.ui.cpu.CPUAnalyzer
+import com.alekso.dltstudio.ui.cpu.CPUSView
 import com.alekso.dltstudio.ui.cpu.CPUUsageEntry
+import com.alekso.dltstudio.ui.cpu.CPUUsageView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -84,10 +96,59 @@ fun TimeLinePanel(
                 }"
             )
 
-            TimelineCPUCView(
-                offset = offset, scale = scale,
-                modifier = Modifier.height(300.dp).fillMaxWidth(), items = dltSession.cpuUsage
+            val state = rememberLazyListState()
+            val panels = mutableStateListOf<@Composable () -> Unit>(
+                {
+                    Row {
+                        Box(modifier = Modifier.width(150.dp)) {
+                            Text("CPU Usage")
+                        }
+                        TimelineCPUCView(
+                            offset = offset,
+                            scale = scale,
+                            modifier = Modifier.height(300.dp).fillMaxWidth(),
+                            items = dltSession.cpuUsage
+                        )
+                    }
+                },
+                {
+                    Row {
+                        Box(modifier = Modifier.width(150.dp)) {
+                            Text("CPU Usage")
+                        }
+                        CPUSView(
+                            modifier = Modifier.height(300.dp).fillMaxWidth().padding(top = 10.dp),
+                            items = dltSession.cpus
+                        )
+                    }
+                },
+                {
+                    Row {
+                        Box(modifier = Modifier.width(150.dp)) {
+                            Text("CPU Usage")
+                        }
+                        CPUSView(
+                            modifier = Modifier.height(300.dp).fillMaxWidth().padding(top = 10.dp),
+                            items = dltSession.cpus
+                        )
+                    }
+                }
             )
+            Box(modifier = Modifier.weight(1f)) {
+                LazyColumn(Modifier, state) {
+                    if (dltSession != null) {
+                        items(panels.size) { i ->
+                            panels[i].invoke()
+                        }
+                    }
+                }
+                VerticalScrollbar(
+                    modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(),
+                    adapter = rememberScrollbarAdapter(
+                        scrollState = state
+                    )
+                )
+            }
         }
     }
 }
