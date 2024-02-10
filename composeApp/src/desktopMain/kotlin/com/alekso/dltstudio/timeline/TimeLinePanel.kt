1ff896a50940490b54305d72083b9bc5ea3a14c4
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2024-02-10 20:22:52 +0100

    Timeline generation moved to TimelineAnalyzer

--- composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/timeline/TimeLinePanel.kt
+++ composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/timeline/TimeLinePanel.kt
@@ -24,7 +24,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -36,26 +35,16 @@ import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.unit.dp
-import com.alekso.dltparser.dlt.DLTMessage
 import com.alekso.dltstudio.ParseSession
-import com.alekso.dltstudio.cpu.CPUAnalyzer
 import com.alekso.dltstudio.cpu.CPUCLegend
-import com.alekso.dltstudio.cpu.CPUSEntry
 import com.alekso.dltstudio.cpu.CPUSLegend
 import com.alekso.dltstudio.cpu.CPUSView
-import com.alekso.dltstudio.cpu.CPUUsageEntry
 import com.alekso.dltstudio.cpu.CPUUsageView
-import com.alekso.dltstudio.memory.MemoryAnalyzer
 import com.alekso.dltstudio.memory.MemoryLegend
-import com.alekso.dltstudio.memory.MemoryUsageEntry
 import com.alekso.dltstudio.memory.MemoryView
-import com.alekso.dltstudio.user.UserAnalyzer
-import com.alekso.dltstudio.user.UserStateEntry
 import com.alekso.dltstudio.user.UserStateLegend
 import com.alekso.dltstudio.user.UserStateView
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -99,60 +88,7 @@ fun TimeLinePanel(
             runClick = {
                 if (dltSession != null) {
                     coroutineScope.launch {
-                        val _cpuUsage = mutableStateListOf<CPUUsageEntry>()
-                        val _cpus = mutableStateListOf<CPUSEntry>()
-                        val _memt = mutableMapOf<String, MutableList<MemoryUsageEntry>>()
-                        val _userState = mutableMapOf<Int, MutableList<UserStateEntry>>()
-                        val _userEntries = mutableMapOf<String, MutableList<TimelineEntry>>()
-
-                        withContext(Dispatchers.IO) {
-
-                            println("Start Timeline building .. ${dltSession.dltMessages.size} messages")
-
-                            dltSession.dltMessages.forEachIndexed { index, message ->
-                                // timeStamps
-                                val ts = message.getTimeStamp()
-                                if (ts > dltSession.timeEnd) {
-                                    dltSession.timeEnd = ts
-                                }
-                                if (ts < dltSession.timeStart) {
-                                    dltSession.timeStart = ts
-                                }
-
-                                analyzeCPUC(message, _cpuUsage, index)
-                                analyzeCPUS(message, _cpus, index)
-                                analyzeMemory(message, index, _memt)
-                                analyzeUserState(message, index, _userState)
-                                progressCallback.invoke((index.toFloat() / dltSession.dltMessages.size))
-                            }
-
-                            // todo: should be user defined and stored on user side
-                            // todo: try split approach - regexp is too slow
-                            val patters = "(?<value>\\d+.\\d+)\\s+%(?<key>(.*)pid\\s*:\\d+)\\("
-                            dltSession.dltMessages.forEachIndexed { index, message ->
-                                TimelineAnalyzer.analyzeEntries(
-                                    message,
-                                    appId = "MON",
-                                    contextId = "CPUP",
-                                    regex = patters.toRegex(),
-                                    map = _userEntries
-                                )
-                                progressCallback.invoke((index.toFloat() / dltSession.dltMessages.size))
-
-                            }
-                        }
-                        withContext(Dispatchers.Default) {
-                            dltSession.cpuUsage.clear()
-                            dltSession.cpuUsage.addAll(_cpuUsage)
-                            dltSession.cpus.clear()
-                            dltSession.cpus.addAll(_cpus)
-                            dltSession.memt.clear()
-                            dltSession.memt = _memt
-                            dltSession.userStateEntries = _userState
-                            dltSession.userEntries = _userEntries
-                            dltSession.totalSeconds =
-                                (dltSession.timeEnd - dltSession.timeStart).toInt() / 1000
-                        }
+                        TimelineAnalyzer.analyzeTimeline(dltSession, progressCallback)
                     }
                 }
             })
@@ -305,87 +241,6 @@ fun TimeLinePanel(
                     )
                 }
             }
-        }
-    }
-}
-
-private fun analyzeCPUC(
-    message: DLTMessage,
-    _cpuUsage: SnapshotStateList<CPUUsageEntry>,
-    index: Int
-) {
-    if (message.ecuId == "MGUA" && message.extendedHeader?.applicationId?.startsWith(
-            "MON"
-        ) == true &&
-        message.extendedHeader?.contextId == "CPUC"
-    ) {
-        try {
-            _cpuUsage.add(CPUAnalyzer.analyzeCPUUsage(index, message))
-        } catch (e: Exception) {
-            // skip
-        }
-    }
-}
-
-private fun analyzeCPUS(
-    message: DLTMessage,
-    _cpus: SnapshotStateList<CPUSEntry>,
-    index: Int
-) {
-    if (message.ecuId == "MGUA" && message.extendedHeader?.applicationId?.startsWith(
-            "MON"
-        ) == true &&
-        message.extendedHeader?.contextId == "CPUS"
-    ) {
-        try {
-            _cpus.add(CPUAnalyzer.analyzeCPUS(index, message))
-        } catch (e: Exception) {
-            // skip
-        }
-    }
-}
-
-private fun analyzeUserState(
-    message: DLTMessage,
-    index: Int,
-    _userState: MutableMap<Int, MutableList<UserStateEntry>>
-) {
-    if (message.ecuId == "MGUA"
-        && message.extendedHeader?.applicationId?.startsWith("ALD") == true
-        && message.extendedHeader?.contextId == "SYST"
-        && message.payload?.asText()?.contains("state changed from") == true
-    ) {
-        try {
-            val userState =
-                UserAnalyzer.analyzeUserStateChanges(index, message)
-            if (!_userState.containsKey(userState.uid)) {
-                _userState[userState.uid] = mutableListOf()
-            }
-            (_userState[userState.uid] as MutableList).add(userState)
-        } catch (e: Exception) {
-            // skip
-        }
-    }
-}
-
-private fun analyzeMemory(
-    message: DLTMessage,
-    index: Int,
-    _memt: MutableMap<String, MutableList<MemoryUsageEntry>>
-) {
-    if (message.ecuId == "MGUA" && message.extendedHeader?.applicationId?.startsWith(
-            "MON"
-        ) == true &&
-        message.extendedHeader?.contextId == "MEMT"
-    ) {
-        try {
-            val memt = MemoryAnalyzer.analyzeMemoryUsage(index, message)
-            if (!_memt.containsKey(memt.name)) {
-                _memt[memt.name] = mutableListOf()
-            }
-            (_memt[memt.name] as MutableList).add(memt)
-        } catch (e: Exception) {
-            // skip
         }
     }
 }
--- composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/timeline/TimelineAnalyzer.kt
+++ composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/timeline/TimelineAnalyzer.kt
@@ -1,11 +1,23 @@
 package com.alekso.dltstudio.timeline
 
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import com.alekso.dltparser.dlt.DLTMessage
 import com.alekso.dltparser.dlt.VerbosePayload
-import com.alekso.dltstudio.timeline.TimelineEntry
+import com.alekso.dltstudio.ParseSession
+import com.alekso.dltstudio.cpu.CPUAnalyzer
+import com.alekso.dltstudio.cpu.CPUSEntry
+import com.alekso.dltstudio.cpu.CPUUsageEntry
+import com.alekso.dltstudio.memory.MemoryAnalyzer
+import com.alekso.dltstudio.memory.MemoryUsageEntry
+import com.alekso.dltstudio.user.UserAnalyzer
+import com.alekso.dltstudio.user.UserStateEntry
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 
 object TimelineAnalyzer {
+
     fun analyzeEntries(
         message: DLTMessage,
         appId: String? = null,
@@ -32,6 +44,144 @@ object TimelineAnalyzer {
             }
         } catch (e: Exception) {
             // ignore
+        }
+    }
+
+    suspend fun analyzeTimeline(dltSession: ParseSession, progressCallback: (Float) -> Unit) {
+        val _cpuUsage = mutableStateListOf<CPUUsageEntry>()
+        val _cpus = mutableStateListOf<CPUSEntry>()
+        val _memt = mutableMapOf<String, MutableList<MemoryUsageEntry>>()
+        val _userState = mutableMapOf<Int, MutableList<UserStateEntry>>()
+        val _userEntries = mutableMapOf<String, MutableList<TimelineEntry>>()
+
+        withContext(Dispatchers.IO) {
+
+            println("Start Timeline building .. ${dltSession.dltMessages.size} messages")
+
+            dltSession.dltMessages.forEachIndexed { index, message ->
+                // timeStamps
+                val ts = message.getTimeStamp()
+                if (ts > dltSession.timeEnd) {
+                    dltSession.timeEnd = ts
+                }
+                if (ts < dltSession.timeStart) {
+                    dltSession.timeStart = ts
+                }
+
+                analyzeCPUC(message, _cpuUsage, index)
+                analyzeCPUS(message, _cpus, index)
+                analyzeMemory(message, index, _memt)
+                analyzeUserState(message, index, _userState)
+                progressCallback.invoke((index.toFloat() / dltSession.dltMessages.size))
+            }
+
+            // todo: should be user defined and stored on user side
+            // todo: try split approach - regexp is too slow
+            val patters = "(?<value>\\d+.\\d+)\\s+%(?<key>(.*)pid\\s*:\\d+)\\("
+            dltSession.dltMessages.forEachIndexed { index, message ->
+                analyzeEntries(
+                    message,
+                    appId = "MON",
+                    contextId = "CPUP",
+                    regex = patters.toRegex(),
+                    map = _userEntries
+                )
+                progressCallback.invoke((index.toFloat() / dltSession.dltMessages.size))
+
+            }
+        }
+        withContext(Dispatchers.Default) {
+            dltSession.cpuUsage.clear()
+            dltSession.cpuUsage.addAll(_cpuUsage)
+            dltSession.cpus.clear()
+            dltSession.cpus.addAll(_cpus)
+            dltSession.memt.clear()
+            dltSession.memt = _memt
+            dltSession.userStateEntries = _userState
+            dltSession.userEntries = _userEntries
+            dltSession.totalSeconds =
+                (dltSession.timeEnd - dltSession.timeStart).toInt() / 1000
+        }
+    }
+
+    private fun analyzeCPUC(
+        message: DLTMessage,
+        _cpuUsage: SnapshotStateList<CPUUsageEntry>,
+        index: Int
+    ) {
+        if (message.ecuId == "MGUA" && message.extendedHeader?.applicationId?.startsWith(
+                "MON"
+            ) == true &&
+            message.extendedHeader?.contextId == "CPUC"
+        ) {
+            try {
+                _cpuUsage.add(CPUAnalyzer.analyzeCPUUsage(index, message))
+            } catch (e: Exception) {
+                // skip
+            }
+        }
+    }
+
+    private fun analyzeCPUS(
+        message: DLTMessage,
+        _cpus: SnapshotStateList<CPUSEntry>,
+        index: Int
+    ) {
+        if (message.ecuId == "MGUA" && message.extendedHeader?.applicationId?.startsWith(
+                "MON"
+            ) == true &&
+            message.extendedHeader?.contextId == "CPUS"
+        ) {
+            try {
+                _cpus.add(CPUAnalyzer.analyzeCPUS(index, message))
+            } catch (e: Exception) {
+                // skip
+            }
+        }
+    }
+
+    private fun analyzeUserState(
+        message: DLTMessage,
+        index: Int,
+        _userState: MutableMap<Int, MutableList<UserStateEntry>>
+    ) {
+        if (message.ecuId == "MGUA"
+            && message.extendedHeader?.applicationId?.startsWith("ALD") == true
+            && message.extendedHeader?.contextId == "SYST"
+            && message.payload?.asText()?.contains("state changed from") == true
+        ) {
+            try {
+                val userState =
+                    UserAnalyzer.analyzeUserStateChanges(index, message)
+                if (!_userState.containsKey(userState.uid)) {
+                    _userState[userState.uid] = mutableListOf()
+                }
+                (_userState[userState.uid] as MutableList).add(userState)
+            } catch (e: Exception) {
+                // skip
+            }
+        }
+    }
+
+    private fun analyzeMemory(
+        message: DLTMessage,
+        index: Int,
+        _memt: MutableMap<String, MutableList<MemoryUsageEntry>>
+    ) {
+        if (message.ecuId == "MGUA" && message.extendedHeader?.applicationId?.startsWith(
+                "MON"
+            ) == true &&
+            message.extendedHeader?.contextId == "MEMT"
+        ) {
+            try {
+                val memt = MemoryAnalyzer.analyzeMemoryUsage(index, message)
+                if (!_memt.containsKey(memt.name)) {
+                    _memt[memt.name] = mutableListOf()
+                }
+                (_memt[memt.name] as MutableList).add(memt)
+            } catch (e: Exception) {
+                // skip
+            }
         }
     }
 }
