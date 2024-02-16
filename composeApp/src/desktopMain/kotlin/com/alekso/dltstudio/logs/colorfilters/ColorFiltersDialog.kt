d2e8e51fd07c8ef5325276e80c37e6cfbe04283e
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2024-02-17 00:17:05 +0100

    CustomCheckbox added

--- composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/logs/colorfilters/ColorFiltersDialog.kt
+++ composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/logs/colorfilters/ColorFiltersDialog.kt
@@ -2,6 +2,7 @@ package com.alekso.dltstudio.logs.colorfilters
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -10,17 +11,19 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogState
 import androidx.compose.ui.window.DialogWindow
 import com.alekso.dltstudio.logs.CellStyle
 import com.alekso.dltstudio.ui.CustomButton
+import com.alekso.dltstudio.ui.CustomCheckbox
 import com.alekso.dltstudio.ui.ImageButton
 
 private val COL_FILTER_NAME_WIDTH_DP = 200.dp
@@ -66,22 +69,28 @@ fun ColorFiltersPanel(
             items(colorFilters.size) { i ->
                 val filter = colorFilters[i]
                 Row(
-                    Modifier.padding(horizontal = 4.dp, vertical = 2.dp),
+                    Modifier.padding(horizontal = 4.dp, vertical = 0.dp),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Text(
-                        text = "text",
-                        modifier = Modifier.width(40.dp)
-                            .background(
-                                color = filter.cellStyle.backgroundColor ?: Color.Transparent
-                            ),
-                        textAlign = TextAlign.Center,
-                        color = filter.cellStyle.textColor ?: Color.Black
+                    var checked by remember { mutableStateOf(true) }
+                    CustomCheckbox(
+                        checked = checked,
+                        modifier = Modifier.padding(horizontal = 8.dp),
+                        onCheckedChange = { checked = !checked }
                     )
-                    Text(
-                        text = filter.name,
-                        Modifier.width(COL_FILTER_NAME_WIDTH_DP).padding(horizontal = 4.dp)
-                    )
+
+                    Box(
+                        modifier = Modifier.background(
+                            color = filter.cellStyle.backgroundColor ?: Color.Transparent
+                        ).padding(horizontal = 4.dp)
+                    ) {
+                        Text(
+                            text = filter.name,
+                            modifier = Modifier.width(COL_FILTER_NAME_WIDTH_DP),
+                            color = filter.cellStyle.textColor ?: Color.Black
+                        )
+                    }
+
                     ImageButton(modifier = Modifier.size(28.dp),
                         iconName = "icon_edit.xml",
                         title = "Edit",
---
+++ composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/ui/CustomCheckbox.kt
@@ -0,0 +1,51 @@
+package com.alekso.dltstudio.ui
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.CheckboxColors
+import androidx.compose.material.CheckboxDefaults
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.LocalMinimumInteractiveComponentEnforcement
+import androidx.compose.material.TriStateCheckbox
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.state.ToggleableState
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun CustomCheckbox(
+    checked: Boolean,
+    onCheckedChange: ((Boolean) -> Unit)?,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    colors: CheckboxColors = CheckboxDefaults.colors(checkedColor = Color.Blue)
+) {
+    CompositionLocalProvider(
+        // disable 48dp padding (minimumInteractiveComponentSize)
+        LocalMinimumInteractiveComponentEnforcement provides false,
+    ) {
+
+        TriStateCheckbox(
+            state = ToggleableState(checked),
+            onClick = if (onCheckedChange != null) {
+                { onCheckedChange(!checked) }
+            } else null,
+            interactionSource = interactionSource,
+            enabled = enabled,
+            colors = colors,
+            modifier = modifier.scale(0.7f) // make hardcoded size smaller
+                .size(16.dp)
+                .requiredSize(16.dp).padding(0.dp)
+
+        )
+    }
+}
+
