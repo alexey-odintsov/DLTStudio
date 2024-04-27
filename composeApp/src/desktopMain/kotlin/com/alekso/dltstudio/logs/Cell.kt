dbccca328b20e0adc8024b3e8f08fe68d82ccaf4
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2024-04-27 16:13:28 +0200

    Cell wrapContent support added

--- composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/logs/Cell.kt
+++ composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/logs/Cell.kt
@@ -1,7 +1,6 @@
 package com.alekso.dltstudio.logs
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
@@ -15,8 +14,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 data class CellStyle(
-    val backgroundColor: Color? = null,
-    val textColor: Color? = null
+    val backgroundColor: Color? = null, val textColor: Color? = null
 ) {
     companion object {
         val Default = CellStyle()
@@ -30,31 +28,34 @@ fun Cell(
     textAlign: TextAlign = TextAlign.Left,
     text: String,
     isHeader: Boolean = false,
-    cellStyle: CellStyle? = null
+    cellStyle: CellStyle? = null,
+    wrapContent: Boolean = false,
 ) {
-    Text(
-        modifier = Modifier
-            .padding(end = 1.dp)
-            .height(12.dp)
-            .background(
-                if (isHeader) {
-                    Color.Transparent
-                } else if (cellStyle != null) {
-                    cellStyle.backgroundColor ?: Color(250, 250, 250)
-                } else {
-                    Color.White
-                }
-            )
-            .then(modifier),
-        maxLines = 1,
-        textAlign = textAlign,
-        fontSize = 10.sp,
-        fontFamily = FontFamily.Monospace,
-        fontWeight = FontWeight(if (isHeader) 600 else 400),
-        softWrap = false,
-        text = text,
-        color = if (cellStyle != null) {
-            cellStyle.textColor ?: Color.Unspecified
-        } else Color.Unspecified
-    )
+    val color = if (cellStyle != null) {
+        cellStyle.textColor ?: Color.Unspecified
+    } else Color.Unspecified
+
+    if (wrapContent) {
+        Text(
+            modifier = Modifier.padding(end = 1.dp).then(modifier),
+            textAlign = textAlign,
+            fontSize = 10.sp,
+            fontFamily = FontFamily.Monospace,
+            fontWeight = FontWeight(if (isHeader) 600 else 400),
+            softWrap = true,
+            text = text,
+            color = color,
+        )
+    } else {
+        Text(
+            modifier = Modifier.padding(end = 1.dp).height(12.dp).then(modifier),
+            maxLines = 1,
+            textAlign = textAlign,
+            fontSize = 10.sp,
+            fontFamily = FontFamily.Monospace,
+            fontWeight = FontWeight(if (isHeader) 600 else 400),
+            text = text,
+            color = color,
+        )
+    }
 }
--- composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/logs/LogRow.kt
+++ composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/logs/LogRow.kt
@@ -2,17 +2,22 @@ package com.alekso.dltstudio.logs
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.Divider
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.alekso.dltstudio.logs.colorfilters.ColorFilterFatal
 
 
 private val selectedCellStyle = CellStyle(backgroundColor = Color.LightGray)
@@ -40,91 +45,146 @@ fun LogRow(
             Modifier.fillMaxWidth()
         )
     ) {
-        val updatedCellStyle = if (isSelected) selectedCellStyle else cellStyle
-        Row {
+        val finalCellStyle =
+            if (isSelected) selectedCellStyle else logTypeIndicator?.logTypeStyle ?: cellStyle
+
+        Row(
+            modifier
+                .height(IntrinsicSize.Max)
+                .background(
+                    if (isHeader) {
+                        Color.Transparent
+                    } else if (finalCellStyle != null) {
+                        finalCellStyle.backgroundColor ?: Color(250, 250, 250)
+                    } else {
+                        Color.White
+                    }
+                )
+        ) {
             Cell(
                 modifier = Modifier.width(54.dp).padding(end = 2.dp),
                 textAlign = TextAlign.Right,
                 text = index,
                 isHeader = isHeader,
-                cellStyle = updatedCellStyle
+                cellStyle = finalCellStyle
             )
+            CellDivider()
             Cell(
                 modifier = Modifier.width(180.dp),
                 textAlign = TextAlign.Center,
                 text = datetime,
                 isHeader = isHeader,
-                cellStyle = updatedCellStyle
+                cellStyle = finalCellStyle
             )
+            CellDivider()
             Cell(
                 modifier = Modifier.width(80.dp).padding(end = 2.dp),
                 textAlign = TextAlign.Right,
                 text = timeOffset,
                 isHeader = isHeader,
-                cellStyle = updatedCellStyle
+                cellStyle = finalCellStyle
             )
+            CellDivider()
             Cell(
                 modifier = Modifier.width(46.dp),
                 textAlign = TextAlign.Center,
                 text = ecu,
                 isHeader = isHeader,
-                cellStyle = updatedCellStyle
+                cellStyle = finalCellStyle
             )
+            CellDivider()
             Cell(
                 modifier = Modifier.width(46.dp),
                 textAlign = TextAlign.Center,
                 text = ecuId,
                 isHeader = isHeader,
-                cellStyle = updatedCellStyle
+                cellStyle = finalCellStyle
             )
+            CellDivider()
             Cell(
                 modifier = Modifier.width(46.dp),
                 textAlign = TextAlign.Center,
                 text = sessionId,
                 isHeader = isHeader,
-                cellStyle = updatedCellStyle
+                cellStyle = finalCellStyle
             )
+            CellDivider()
             Cell(
                 modifier = Modifier.width(46.dp),
                 textAlign = TextAlign.Center,
                 text = applicationId,
                 isHeader = isHeader,
-                cellStyle = updatedCellStyle
+                cellStyle = finalCellStyle
             )
+            CellDivider()
             Cell(
                 modifier = Modifier.width(46.dp),
                 textAlign = TextAlign.Center,
                 text = contextId,
                 isHeader = isHeader,
-                cellStyle = updatedCellStyle
+                cellStyle = finalCellStyle
             )
+            CellDivider()
             Cell(
                 modifier = Modifier.width(14.dp),
                 text = logTypeIndicator?.logTypeSymbol ?: "",
                 textAlign = TextAlign.Center,
                 isHeader = isHeader,
-                cellStyle = logTypeIndicator?.logTypeStyle ?: updatedCellStyle
+                cellStyle = logTypeIndicator?.logTypeStyle ?: finalCellStyle
             )
+            CellDivider()
             Cell(
                 modifier = Modifier.weight(1f).padding(start = 6.dp),
                 text = content,
                 isHeader = isHeader,
-                cellStyle = updatedCellStyle
+                cellStyle = finalCellStyle
             )
         }
-        Divider()
+        RowDivider()
     }
+}
 
+@Composable
+fun CellDivider() {
+    Box(
+        Modifier
+            .fillMaxHeight()
+            .width(1.dp)
+            .background(color = Color.LightGray)
+    )
+}
+
+@Composable
+fun RowDivider() {
+    Box(
+        Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+            .background(color = Color.LightGray)
+    )
 }
 
 @Preview
 @Composable
 fun LogRowPreview() {
+    val contents = listOf(
+        "Content",
+        "Another string with _ character",
+        "ÄÜË",
+        "0123456789-+=<>/?",
+        "汉语",
+        "~`!@#$%^&*()_+",
+        "Fatal error",
+        "",
+        "",
+        "This is the logs content strings, that doesn't fit in one line, let's repeat, This is the logs content strings, that doesn't fit in one line, let's repeat",
+    )
     Column(modifier = Modifier.background(Color.Gray)) {
-        (1..10).forEach { i ->
+        (0..9).forEach { i ->
             LogRow(
-                modifier = Modifier.fillMaxWidth(),
-                isSelected = false,
+                modifier = Modifier.fillMaxWidth().wrapContentHeight(),
+                isSelected = i == 3,
+                isHeader = i == 0,
                 index = (16_345_345 + i).toString(),
                 datetime = "2024-02-04 18:26:23.074689",
                 timeOffset = "1234",
@@ -133,7 +193,20 @@ fun LogRowPreview() {
                 applicationId = "AppId",
                 ecuId = "EcuId",
                 contextId = "Con",
-                content = "Content goes here",
+                content = contents[i],
+                cellStyle = when (i) {
+                    9 -> CellStyle(backgroundColor = Color.Yellow)
+                    8 -> CellStyle(
+                        backgroundColor = Color(0xE7, 0x62, 0x29),
+                        textColor = Color.White
+                    )
+
+                    else -> null
+                },
+                logTypeIndicator = if (i == 6) LogTypeIndicator(
+                    "F",
+                    ColorFilterFatal.cellStyle
+                ) else null
             )
         }
     }
