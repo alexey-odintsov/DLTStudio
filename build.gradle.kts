d437ad7744881dae2e7c7ba51a89b92d65681288
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2024-05-27 10:59:00 +0200

    Kotlin 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
