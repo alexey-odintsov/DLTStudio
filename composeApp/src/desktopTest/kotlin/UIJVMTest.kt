f7892a98beffaabc5ae6f1dc0e937f02b20ea4f2
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2024-05-28 12:22:28 +0200

    Update UI test

--- composeApp/src/desktopTest/kotlin/UIJVMTest.kt
+++ composeApp/src/desktopTest/kotlin/UIJVMTest.kt
@@ -1,15 +1,6 @@
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
-import com.alekso.dltparser.DLTParserV2
-import com.alekso.dltstudio.MainViewModel
-import com.alekso.dltstudio.timeline.TimelineViewModel
-import com.alekso.dltstudio.ui.MainWindow
+import com.alekso.dltstudio.ui.TabsPanel
 import org.junit.Rule
 import org.junit.Test
 
@@ -20,19 +11,10 @@ class UIJVMTest {
     @Test
     fun `test tabs presence`() {
         rule.setContent {
-            var progress by remember { mutableStateOf(0f) }
-            val onProgressUpdate: (Float) -> Unit = { i -> progress = i }
-
-            val mainViewModel = remember { MainViewModel(DLTParserV2(), onProgressUpdate) }
-            val timelineViewModel = remember { TimelineViewModel(onProgressUpdate) }
-
-            MainWindow(mainViewModel, timelineViewModel, progress, onProgressUpdate)
+            TabsPanel(0, listOf("Logs", "Timeline"), { })
         }
 
-        rule.onNodeWithText("Logs").performClick()
-        rule.onNodeWithContentDescription("Search").assertExists("Search button was not found",)
-        rule.onNodeWithText("Timeline").performClick()
-        rule.onNodeWithContentDescription("Analyze timeline").assertExists("Analyze button was not found",)
-        rule.onNodeWithContentDescription("Timeline filters").assertExists("Timeline filters button was not found",)
+        rule.onNodeWithText("Logs").assertExists("Logs tab doesn't exist")
+        rule.onNodeWithText("Timeline").assertExists("Timeline tab doesn't exist")
     }
 }
