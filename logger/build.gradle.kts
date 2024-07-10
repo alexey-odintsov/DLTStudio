3753d870b94a6767d1ac5c3255917cb3fdc32f01
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2024-07-10 20:30:56 +0200

    Remove java version for logger module

--- logger/build.gradle.kts
+++ logger/build.gradle.kts
@@ -2,8 +2,3 @@ plugins {
     id("java-library")
     alias(libs.plugins.org.jetbrains.kotlin.jvm)
 }
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
-}
