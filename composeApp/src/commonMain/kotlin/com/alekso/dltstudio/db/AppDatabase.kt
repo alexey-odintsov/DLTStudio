04aea554af0e6c4b7264020b3a279c29afe28af8
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2024-08-25 15:26:30 +0200

    Add database schema

--- composeApp/src/commonMain/kotlin/com/alekso/dltstudio/db/AppDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/alekso/dltstudio/db/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.alekso.dltstudio.db.virtualdevice.VirtualDeviceDao
 import com.alekso.dltstudio.db.virtualdevice.VirtualDeviceEntity
 
-@Database(entities = [VirtualDeviceEntity::class], version = 1, exportSchema = false)
+@Database(entities = [VirtualDeviceEntity::class], version = 1, exportSchema = true)
 abstract class AppDatabase: RoomDatabase() {
 
     abstract fun getVirtualDeviceDao(): VirtualDeviceDao
