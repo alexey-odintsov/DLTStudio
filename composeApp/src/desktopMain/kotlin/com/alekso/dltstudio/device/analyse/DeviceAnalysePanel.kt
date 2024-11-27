089e3816ec7bc5e0dad8d7c29f3425c711c42309
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2024-11-27 23:04:07 +0100

    Predefined adb shell commands added

--- composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/device/analyse/DeviceAnalysePanel.kt
+++ composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/device/analyse/DeviceAnalysePanel.kt
@@ -8,11 +8,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -22,6 +24,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.alekso.dltstudio.ui.CustomButton
+import com.alekso.dltstudio.ui.CustomDropDown
 import com.alekso.dltstudio.ui.CustomEditText
 
 @Composable
@@ -33,13 +36,30 @@ fun DeviceAnalysePanel(
     val responseState = deviceAnalyzeViewModel.analyzeState
     var cmd by rememberSaveable { mutableStateOf("adb devices") }
     val scroll = rememberScrollState(0)
+    val predefinedCommands = remember { listOf(
+        "adb devices",
+        "adb shell dumpsys -l",
+        "adb shell dumpsys meminfo",
+        "adb shell dumpsys meminfo package_name|pid [-d]",
+        "adb shell dumpsys procstats --hours 1",
+        "adb shell dumpsys gfxinfo package-name",
+        "adb shell dumpsys gfxinfo package-name framestats",
+    ) }
 
     Column(modifier = modifier) {
         Column {
             Text(modifier = textStyle, text = "Command:")
             Row {
+                CustomDropDown(
+                    modifier = Modifier.weight(1f).padding(horizontal = 4.dp),
+                    items = predefinedCommands,
+                    initialSelectedIndex = 0,
+                    onItemsSelected = { index -> cmd = predefinedCommands[index] }
+                )
+            }
+            Row {
                 CustomEditText(
-                    modifier = Modifier.padding(4.dp).weight(1f).height(60.dp),
+                    modifier = Modifier.padding(vertical = 4.dp).weight(1f).height(60.dp),
                     singleLine = false,
                     value = cmd, onValueChange = {
                         cmd = it
@@ -56,14 +76,16 @@ fun DeviceAnalysePanel(
         }
 
         Row(verticalAlignment = Alignment.CenterVertically) {
-            Text(
-                fontFamily = FontFamily.Monospace,
-                fontSize = 10.sp,
-                modifier = Modifier.padding(4.dp).fillMaxSize().background(Color.White)
-                    .height(100.dp).wrapContentHeight(Alignment.Top)
-                    .verticalScroll(scroll)
-                    .padding(horizontal = 4.dp), text = responseState.joinToString("\n")
-            )
+            SelectionContainer {
+                Text(
+                    fontFamily = FontFamily.Monospace,
+                    fontSize = 10.sp,
+                    modifier = Modifier.padding(4.dp).fillMaxSize().background(Color.White)
+                        .height(100.dp).wrapContentHeight(Alignment.Top)
+                        .verticalScroll(scroll)
+                        .padding(horizontal = 4.dp), text = responseState.joinToString("\n")
+                )
+            }
         }
     }
 }
