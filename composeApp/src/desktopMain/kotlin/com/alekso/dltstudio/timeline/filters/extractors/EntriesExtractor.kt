26f7b660cd40a005f1c010f962ee73e209d31b12
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2024-12-06 23:36:10 +0100

    Cleanup extractors

--- composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/timeline/filters/extractors/EntriesExtractor.kt
+++ composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/timeline/filters/extractors/EntriesExtractor.kt
@@ -10,16 +10,10 @@ import com.alekso.dltstudio.timeline.TimeLineEntry
 import com.alekso.dltstudio.timeline.TimeLineEvent
 import com.alekso.dltstudio.timeline.TimeLineEventEntries
 import com.alekso.dltstudio.timeline.TimeLineEventEntry
-import com.alekso.dltstudio.timeline.TimeLineFloatEntry
-import com.alekso.dltstudio.timeline.TimeLineMinMaxEntries
-import com.alekso.dltstudio.timeline.TimeLinePercentageEntries
 import com.alekso.dltstudio.timeline.TimeLineSingleStateEntries
 import com.alekso.dltstudio.timeline.TimeLineSingleStateEntry
-import com.alekso.dltstudio.timeline.TimeLineStateEntries
-import com.alekso.dltstudio.timeline.TimeLineStateEntry
 import com.alekso.dltstudio.timeline.filters.NO_KEY
 import com.alekso.dltstudio.timeline.filters.TimelineFilter
-import kotlin.text.get
 
 interface EntriesExtractor {
     interface ExtractionType
@@ -53,60 +47,9 @@ class NonNamedEntriesExtractor : EntriesExtractorOld {
         val matches = regex.find(message.payload)!!
 
         when (filter.diagramType) {
-            DiagramType.Percentage -> {// todo: Mode extraction to Diagram implementations
-                if (matches.groups.size > 2) {
-                    for (i in 1..<matches.groups.size step 2) {
-                        val key = matches.groups[i]?.value
-                        val value = matches.groups[i + 1]?.value
-                        if (key != null && value != null) {
-                            (entries as TimeLinePercentageEntries).addEntry(
-                                TimeLineFloatEntry(
-                                    message.timeStampNano,
-                                    key,
-                                    value.toFloat()
-                                )
-                            )
-                        }
-                    }
-                }
-            }
-
-            DiagramType.MinMaxValue -> {
-                if (matches.groups.size > 2) {
-                    for (i in 1..<matches.groups.size step 2) {
-                        val key = matches.groups[i]?.value
-                        val value = matches.groups[i + 1]?.value
-                        if (key != null && value != null) {
-                            (entries as TimeLineMinMaxEntries).addEntry(
-                                TimeLineFloatEntry(
-                                    message.timeStampNano,
-                                    key,
-                                    value.toFloat()
-                                )
-                            )
-                        }
-                    }
-                }
-            }
-
-            DiagramType.State -> {
-                if (matches.groups.size > 2) {
-                    for (i in 1..3) {
-                        val key = matches.groups[i]?.value
-                        val value = matches.groups[i + 1]?.value
-                        val oldValue = matches.groups[i + 2]?.value
-                        if (key != null && value != null && oldValue != null) {
-                            (entries as TimeLineStateEntries).addEntry(
-                                TimeLineStateEntry(
-                                    message.timeStampNano,
-                                    key,
-                                    Pair(value, oldValue)
-                                )
-                            )
-                        }
-                    }
-                }
-            }
+            DiagramType.Percentage -> {}
+            DiagramType.MinMaxValue -> {}
+            DiagramType.State -> {}
 
             DiagramType.SingleState -> {
                 if (matches.groups.size > 2) {
@@ -161,38 +104,9 @@ class NamedEntriesExtractor : EntriesExtractorOld {
         val diagramParams = filter.diagramType.params
 
         when (filter.diagramType) {
-            DiagramType.Percentage -> {
-                val key: String = matches.groups[diagramParams[Param.KEY]?.key!!]?.value ?: NO_KEY
-                val value: String? = matches.groups[diagramParams[Param.VALUE]?.key!!]?.value
-
-                if (value != null) {
-                    (entries as TimeLinePercentageEntries).addEntry(
-                        TimeLineFloatEntry(
-                            message.timeStampNano,
-                            key,
-                            value.toFloat()
-                        )
-                    )
-                }
-            }
-
-            DiagramType.MinMaxValue -> {
-                val key: String = matches.groups[diagramParams[Param.KEY]?.key!!]?.value ?: NO_KEY
-                val value: String? = matches.groups[diagramParams[Param.VALUE]?.key!!]?.value
-
-                if (value != null) {
-                    (entries as TimeLineMinMaxEntries).addEntry(
-                        TimeLineFloatEntry(
-                            message.timeStampNano,
-                            key,
-                            value.toFloat()
-                        )
-                    )
-                }
-            }
-
-            DiagramType.State -> {
-            }
+            DiagramType.Percentage -> {}
+            DiagramType.MinMaxValue -> {}
+            DiagramType.State -> {}
 
             DiagramType.SingleState -> {
                 val key: String = matches.groups[diagramParams[Param.KEY]?.key!!]?.value ?: NO_KEY
