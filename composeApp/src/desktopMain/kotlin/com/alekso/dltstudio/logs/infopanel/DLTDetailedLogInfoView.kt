2e9732adb9cd964539554b731f7da2727773a29c
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2025-01-15 00:53:20 +0100

    Put back DetailedView

--- composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/logs/infopanel/DLTDetailedLogInfoView.kt
+++ composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/logs/infopanel/DLTDetailedLogInfoView.kt
@@ -43,101 +43,101 @@ fun DLTDetailedInfoView(
                     text = "Standard header:"
                 )
 
-//                TableRow(
-//                    parameterRowWidth, "Header Type",
-//                    "0x${it.standardHeader.headerType.originalByte.toHex()} " +
-//                            "(${it.standardHeader.headerType.originalByte.toBinary(8)}b)"
-//                )
-//                TableRow(
-//                    parameterRowWidth, "  Extender header",
-//                    "${it.standardHeader.headerType.useExtendedHeader}"
-//                )
-//                TableRow(
-//                    parameterRowWidth, "  Payload Endian",
-//                    if (it.standardHeader.headerType.payloadBigEndian) "BIG" else "LITTLE"
-//                )
-//                TableRow(
-//                    parameterRowWidth, "  ECU present",
-//                    "${it.standardHeader.headerType.withEcuId}"
-//                )
-//                TableRow(
-//                    parameterRowWidth, "  Session present",
-//                    "${it.standardHeader.headerType.withSessionId}"
-//                )
-//                TableRow(
-//                    parameterRowWidth, "  Timestamp present",
-//                    "${it.standardHeader.headerType.withTimestamp}"
-//                )
-//                TableRow(
-//                    parameterRowWidth, "  Version number",
-//                    "0x${it.standardHeader.headerType.versionNumber.toHex()}"
-//                )
-//
-//                TableRow(
-//                    parameterRowWidth, "Message counter",
-//                    "${it.standardHeader.messageCounter}"
-//                )
-//                TableRow(
-//                    parameterRowWidth, "Length",
-//                    "${it.standardHeader.length}"
-//                )
-//                if (it.standardHeader.headerType.withEcuId) {
-//                    TableRow(
-//                        parameterRowWidth, "ECU Id",
-//                        "'${it.standardHeader.ecuId}'"
-//                    )
-//                }
-//                if (it.standardHeader.headerType.withSessionId) {
-//                    TableRow(
-//                        parameterRowWidth, "Session Id",
-//                        "${it.standardHeader.sessionId}"
-//                    )
-//                }
-//
-//                if (it.standardHeader.headerType.withTimestamp) {
-//                    TableRow(
-//                        parameterRowWidth, "Timestamp",
-//                        "${it.standardHeader.timeStamp}"
-//                    )
-//                }
-//
-//                if (it.extendedHeader != null) {
-//                    val extendedHeader: ExtendedHeader = it.extendedHeader!!
-//                    Header(
-//                        modifier = paddingModifier,
-//                        text = "Extended header:"
-//                    )
-//                    TableRow(
-//                        parameterRowWidth, "  Message info",
-//                        "0x${extendedHeader.messageInfo.originalByte.toHex()} " +
-//                                "(${extendedHeader.messageInfo.originalByte.toBinary(8)}b)"
-//                    )
-//                    TableRow(
-//                        parameterRowWidth, "  Verbose",
-//                        "${extendedHeader.messageInfo.verbose}"
-//                    )
-//                    TableRow(
-//                        parameterRowWidth, "  Message type",
-//                        "${extendedHeader.messageInfo.messageType}"
-//                    )
-//                    TableRow(
-//                        parameterRowWidth, "  Message type info",
-//                        "${extendedHeader.messageInfo.messageTypeInfo}"
-//                    )
-//                    TableRow(
-//                        parameterRowWidth, "Arguments count",
-//                        "${extendedHeader.argumentsCount}"
-//                    )
-//                    TableRow(
-//                        parameterRowWidth, "Application Id",
-//                        "'${extendedHeader.applicationId}'"
-//                    )
-//                    TableRow(
-//                        parameterRowWidth, "Context Id",
-//                        "'${extendedHeader.contextId}'"
-//                    )
-//
-//                }
+                TableRow(
+                    parameterRowWidth, "Header Type",
+                    "0x${it.standardHeader.headerType.originalByte.toHex()} " +
+                            "(${it.standardHeader.headerType.originalByte.toBinary(8)}b)"
+                )
+                TableRow(
+                    parameterRowWidth, "  Extender header",
+                    "${it.standardHeader.headerType.useExtendedHeader}"
+                )
+                TableRow(
+                    parameterRowWidth, "  Payload Endian",
+                    if (it.standardHeader.headerType.payloadBigEndian) "BIG" else "LITTLE"
+                )
+                TableRow(
+                    parameterRowWidth, "  ECU present",
+                    "${it.standardHeader.headerType.withEcuId}"
+                )
+                TableRow(
+                    parameterRowWidth, "  Session present",
+                    "${it.standardHeader.headerType.withSessionId}"
+                )
+                TableRow(
+                    parameterRowWidth, "  Timestamp present",
+                    "${it.standardHeader.headerType.withTimestamp}"
+                )
+                TableRow(
+                    parameterRowWidth, "  Version number",
+                    "0x${it.standardHeader.headerType.versionNumber.toHex()}"
+                )
+
+                TableRow(
+                    parameterRowWidth, "Message counter",
+                    "${it.standardHeader.messageCounter}"
+                )
+                TableRow(
+                    parameterRowWidth, "Length",
+                    "${it.standardHeader.length}"
+                )
+                if (it.standardHeader.headerType.withEcuId) {
+                    TableRow(
+                        parameterRowWidth, "ECU Id",
+                        "'${it.standardHeader.ecuId}'"
+                    )
+                }
+                if (it.standardHeader.headerType.withSessionId) {
+                    TableRow(
+                        parameterRowWidth, "Session Id",
+                        "${it.standardHeader.sessionId}"
+                    )
+                }
+
+                if (it.standardHeader.headerType.withTimestamp) {
+                    TableRow(
+                        parameterRowWidth, "Timestamp",
+                        "${it.standardHeader.timeStamp}"
+                    )
+                }
+
+                if (it.extendedHeader != null) {
+                    val extendedHeader: ExtendedHeader = it.extendedHeader!!
+                    Header(
+                        modifier = paddingModifier,
+                        text = "Extended header:"
+                    )
+                    TableRow(
+                        parameterRowWidth, "  Message info",
+                        "0x${extendedHeader.messageInfo.originalByte.toHex()} " +
+                                "(${extendedHeader.messageInfo.originalByte.toBinary(8)}b)"
+                    )
+                    TableRow(
+                        parameterRowWidth, "  Verbose",
+                        "${extendedHeader.messageInfo.verbose}"
+                    )
+                    TableRow(
+                        parameterRowWidth, "  Message type",
+                        "${extendedHeader.messageInfo.messageType}"
+                    )
+                    TableRow(
+                        parameterRowWidth, "  Message type info",
+                        "${extendedHeader.messageInfo.messageTypeInfo}"
+                    )
+                    TableRow(
+                        parameterRowWidth, "Arguments count",
+                        "${extendedHeader.argumentsCount}"
+                    )
+                    TableRow(
+                        parameterRowWidth, "Application Id",
+                        "'${extendedHeader.applicationId}'"
+                    )
+                    TableRow(
+                        parameterRowWidth, "Context Id",
+                        "'${extendedHeader.contextId}'"
+                    )
+
+                }
 
                 Header(
                     modifier = paddingModifier,
