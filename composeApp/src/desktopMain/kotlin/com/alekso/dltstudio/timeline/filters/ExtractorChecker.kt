dd30a406a675f0e0abcee2e300eeb15e269f7ca4
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2025-01-24 20:35:49 +0100

    Reuse EntriesExtractor for ExtractorChecker

--- composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/timeline/filters/ExtractorChecker.kt
+++ composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/timeline/filters/ExtractorChecker.kt
@@ -1,13 +1,7 @@
 package com.alekso.dltstudio.timeline.filters
 
 import com.alekso.dltstudio.timeline.DiagramType
-import com.alekso.dltstudio.timeline.filters.extractors.DurationEntriesExtractor
 import com.alekso.dltstudio.timeline.filters.extractors.EntriesExtractor
-import com.alekso.dltstudio.timeline.filters.extractors.EventEntriesExtractor
-import com.alekso.dltstudio.timeline.filters.extractors.MinMaxEntriesExtractor
-import com.alekso.dltstudio.timeline.filters.extractors.PercentageEntriesExtractor
-import com.alekso.dltstudio.timeline.filters.extractors.SingleStateEntriesExtractor
-import com.alekso.dltstudio.timeline.filters.extractors.StateEntriesExtractor
 import com.alekso.dltstudio.utils.SampleData
 
 object ExtractorChecker {
@@ -16,67 +10,25 @@ object ExtractorChecker {
         testPayload: String?,
         extractorType: EntriesExtractor.ExtractionType,
         diagramType: DiagramType,
-        global: Boolean = false
     ): String {
-        val testMessage = SampleData.create(payloadText = testPayload)
-        var groupsTestValue = ""
-        if (extractPattern != null && testPayload != null) {
-            val extractPatternRegex = Regex(extractPattern)
-            try {
-                when (diagramType) {
-                    DiagramType.Percentage -> {
-                        PercentageEntriesExtractor().extractEntry(
-                            testMessage, extractPatternRegex, extractorType
-                        ).map { e ->
-                            groupsTestValue = "${e.key} -> ${e.value}"
-                        }
-                    }
-
-                    DiagramType.MinMaxValue -> {
-                        MinMaxEntriesExtractor().extractEntry(
-                            testMessage, extractPatternRegex, extractorType
-                        ).map { e ->
-                            groupsTestValue = "${e.key} -> ${e.value}"
-                        }
-                    }
-
-                    DiagramType.State -> {
-                        StateEntriesExtractor().extractEntry(
-                            testMessage, extractPatternRegex, extractorType
-                        ).map { e ->
-                            groupsTestValue = "${e.key} -> ${e.value.first} / ${e.value.second}"
-                        }
-                    }
-
-
-                    DiagramType.SingleState -> {
-                        SingleStateEntriesExtractor().extractEntry(
-                            testMessage, extractPatternRegex, extractorType
-                        ).map { e ->
-                            groupsTestValue = "${e.key} -> ${e.value}"
-                        }
-                    }
-
-                    DiagramType.Duration -> {
-                        DurationEntriesExtractor().extractEntry(
-                            testMessage, extractPatternRegex, extractorType
-                        ).map { e ->
-                            groupsTestValue = "${e.key} -> ${e.value.first} / ${e.value.second}"
-                        }
-                    }
-
-                    DiagramType.Events -> {
-                        EventEntriesExtractor().extractEntry(
-                            testMessage, extractPatternRegex, extractorType
-                        ).map { e ->
-                            groupsTestValue = "${e.key} -> ${e.value} / ${e.value.info}"
-                        }
-                    }
-                }
-            } catch (e: Exception) {
-                groupsTestValue = "Invalid regex ${e.printStackTrace()}"
+        try {
+            if (extractPattern == null) {
+                return "Empty extractor pattern"
             }
+            val testMessage = SampleData.create(payloadText = testPayload)
+
+            val entries = diagramType.createEntries()
+            EntriesExtractor.analyzeEntriesRegex(
+                testMessage,
+                diagramType,
+                extractorType,
+                Regex(extractPattern),
+                entries
+            )
+
+            return entries.map.toString()
+        } catch (e: Exception) {
+            return "Can't extract entry"
         }
-        return groupsTestValue
     }
 }
--- composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/timeline/filters/extractors/EntriesExtractor.kt
+++ composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/timeline/filters/extractors/EntriesExtractor.kt
@@ -1,7 +1,15 @@
 package com.alekso.dltstudio.timeline.filters.extractors
 
 import com.alekso.dltparser.dlt.DLTMessage
+import com.alekso.dltstudio.timeline.DiagramType
+import com.alekso.dltstudio.timeline.TimeLineDurationEntries
+import com.alekso.dltstudio.timeline.TimeLineEntries
 import com.alekso.dltstudio.timeline.TimeLineEntry
+import com.alekso.dltstudio.timeline.TimeLineEventEntries
+import com.alekso.dltstudio.timeline.TimeLineMinMaxEntries
+import com.alekso.dltstudio.timeline.TimeLinePercentageEntries
+import com.alekso.dltstudio.timeline.TimeLineSingleStateEntries
+import com.alekso.dltstudio.timeline.TimeLineStateEntries
 
 interface EntriesExtractor {
     enum class ExtractionType(val description: String) {
@@ -30,4 +38,56 @@ interface EntriesExtractor {
         regex: Regex,
         extractionType: ExtractionType,
     ): List<TimeLineEntry<*>>
+
+    companion object {
+        fun analyzeEntriesRegex(
+            message: DLTMessage,
+            diagramType: DiagramType,
+            extractorType: ExtractionType,
+            regex: Regex,
+            entries: TimeLineEntries<*>
+        ) {
+            try {
+                when (diagramType) {
+                    DiagramType.Percentage -> {
+                        PercentageEntriesExtractor().extractEntry(
+                            message, regex, extractorType
+                        ).forEach { e -> (entries as TimeLinePercentageEntries).addEntry(e) }
+                    }
+
+                    DiagramType.MinMaxValue -> {
+                        MinMaxEntriesExtractor().extractEntry(
+                            message, regex, extractorType
+                        ).forEach { e -> (entries as TimeLineMinMaxEntries).addEntry(e) }
+                    }
+
+                    DiagramType.State -> {
+                        StateEntriesExtractor().extractEntry(
+                            message, regex, extractorType
+                        ).forEach { e -> (entries as TimeLineStateEntries).addEntry(e) }
+                    }
+
+                    DiagramType.SingleState -> {
+                        SingleStateEntriesExtractor().extractEntry(
+                            message, regex, extractorType
+                        ).forEach { e -> (entries as TimeLineSingleStateEntries).addEntry(e) }
+                    }
+
+                    DiagramType.Duration -> {
+                        DurationEntriesExtractor().extractEntry(
+                            message, regex, extractorType
+                        ).forEach { e -> (entries as TimeLineDurationEntries).addEntry(e) }
+                    }
+
+                    DiagramType.Events -> {
+                        EventEntriesExtractor().extractEntry(
+                            message, regex, extractorType
+                        ).forEach { e -> (entries as TimeLineEventEntries).addEntry(e) }
+                    }
+                }
+            } catch (e: Exception) {
+                // ignore
+            }
+        }
+    }
 }
