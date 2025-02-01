f87c0a0e7c389355c86bd3c5c14edba1841dc291
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2025-02-01 23:25:49 +0100

    Mark Plugin as Stable to make renderPanel skippable

--- composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/plugins/PluginPanel.kt
+++ composeApp/src/desktopMain/kotlin/com/alekso/dltstudio/plugins/PluginPanel.kt
@@ -1,9 +1,11 @@
 package com.alekso.dltstudio.plugins
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
 
 
+@Stable
 interface PluginPanel {
     fun getPanelName(): String
 
