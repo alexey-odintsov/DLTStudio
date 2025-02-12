45ecc3602837964ea7c66006aa578601ae1c59dc
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2025-02-12 19:54:33 +0100

    Fix jvm

--- predefinedPlugins/build.gradle.kts
+++ predefinedPlugins/build.gradle.kts
@@ -1,16 +1,24 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
 }
 
 kotlin {
     jvm("desktop")
 
     sourceSets {
+        val desktopMain by getting
+
         commonMain.dependencies {
             implementation(project(":plugins"))
             implementation(project(":pluginFilesViewer"))
             implementation(project(":pluginDeviceAnalyze"))
+        }
+
+        desktopMain.dependencies {
+            implementation(compose.desktop.currentOs)
         }
     }
 }
