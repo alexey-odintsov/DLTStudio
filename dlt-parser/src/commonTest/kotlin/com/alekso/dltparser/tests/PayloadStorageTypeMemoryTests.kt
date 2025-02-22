3efa3cc7290cebbe269b092245e89db10e8ba058
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2025-02-22 15:21:36 +0100

    Fix tests

--- dlt-parser/src/commonTest/kotlin/com/alekso/dltparser/tests/PayloadStorageTypeMemoryTests.kt
+++ dlt-parser/src/commonTest/kotlin/com/alekso/dltparser/tests/PayloadStorageTypeMemoryTests.kt
@@ -1,7 +1,7 @@
 package com.alekso.dltparser.tests
 
-import com.alekso.dltparser.DLTParserV2
 import com.alekso.dltmessage.PayloadStorageType
+import com.alekso.dltparser.DLTParserV2
 import kotlinx.coroutines.runBlocking
 import org.junit.Ignore
 import org.junit.Test
@@ -10,7 +10,7 @@ import java.io.File
 @Ignore("Ignore test for CI")
 class PayloadStorageTypeMemoryTests {
     val payloadStorageType = PayloadStorageType.Binary
-    val parser = DLTParserV2(payloadStorageType)
+    val parser = DLTParserV2()
 
     @Test
     fun `PayloadStorageType memory usage test`() = runBlocking {
@@ -18,7 +18,9 @@ class PayloadStorageTypeMemoryTests {
         runtime.gc()
         val beforeMemory = runtime.totalMemory() - runtime.freeMemory()
         println("Memory before parsing $beforeMemory")
-        val messages = parser.read({}, listOf(File("${System.getProperty("user.home")}/Downloads/dlt/937mb_trace.dlt")))
+        val messages = parser.read(
+            listOf(File("${System.getProperty("user.home")}/Downloads/dlt/937mb_trace.dlt")),
+            payloadStorageType, {})
         runtime.gc()
         val afterMemory = runtime.totalMemory() - runtime.freeMemory()
         val memoryUsed = afterMemory - beforeMemory
