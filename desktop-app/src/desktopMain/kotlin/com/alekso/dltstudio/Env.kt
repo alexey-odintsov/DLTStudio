254b8e66ed921dceda91c27a5108532f4e8d17e5
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2025-02-23 10:31:33 +0100

    Plugins settings panel added

--- desktop-app/src/desktopMain/kotlin/com/alekso/dltstudio/Env.kt
+++ desktop-app/src/desktopMain/kotlin/com/alekso/dltstudio/Env.kt
@@ -6,6 +6,7 @@ object Env {
     private const val PREFERENCES_FILE = "preferences.txt"
     private const val DB_FOLDER = "db"
     private const val DB_FILE = "dlt_studio.db"
+    private const val PLUGINS_FOLDER = "plugins"
 
     private fun getAppPath() = "${getUserHome()}/$APP_FOLDER"
     private fun getUserHome() = System.getProperty("user.home")
@@ -13,4 +14,5 @@ object Env {
     fun getDbPath() = "${getAppPath()}/$DB_FOLDER/$DB_FILE"
     fun getPreferencesPath() = "${getAppPath()}/$PREFERENCES_FILE"
     fun getLogsPath() = "${getAppPath()}/$LOG_FILE"
+    fun getPluginsPath() = "${getAppPath()}/$PLUGINS_FOLDER"
 }
--- desktop-app/src/desktopMain/kotlin/com/alekso/dltstudio/settings/PluginsPanel.kt
+++ desktop-app/src/desktopMain/kotlin/com/alekso/dltstudio/settings/PluginsPanel.kt
@@ -1,9 +1,35 @@
 package com.alekso.dltstudio.settings
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.alekso.dltstudio.plugins.DependencyManager
 
 @Composable
 fun PluginsPanel(callbacks: SettingsDialogCallbacks) {
-    Text("Plugins")
+    val pluginManager = remember { DependencyManager.providePluginsManager() }
+    Text(
+        text = "Plugins",
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(bottom = 10.dp)
+    )
+    Text("Plugins path:")
+    Text(pluginManager.pluginsPath)
+
+    Spacer(Modifier.height(6.dp))
+
+    Text("Registered plugins:")
+    Column {
+        DependencyManager.providePluginsManager().plugins.forEach { plugin ->
+            Text("${plugin.pluginName()} ${plugin.pluginVersion()}")
+        }
+    }
+
 }
