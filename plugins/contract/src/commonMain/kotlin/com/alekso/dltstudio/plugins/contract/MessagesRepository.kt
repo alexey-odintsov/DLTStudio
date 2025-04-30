5db79415fc057a45e0fea2fbe0e350d9c5be4788
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2025-04-30 23:34:29 +0200

    Interface doc added

--- plugins/contract/src/commonMain/kotlin/com/alekso/dltstudio/plugins/contract/MessagesRepository.kt
+++ plugins/contract/src/commonMain/kotlin/com/alekso/dltstudio/plugins/contract/MessagesRepository.kt
@@ -12,8 +12,25 @@ interface MessagesRepository {
     fun getSearchResults(): SnapshotStateList<LogMessage>
     fun getSearchIndexes(): SnapshotStateList<Int>
     fun getMessageByIndex(index: Int): LogMessage
+
+    /**
+     * Sets comment for a message
+     * @param key - ket of the message
+     * @param comment - comment to set. null to remove comment.
+     */
     fun updateLogComment(key: String, comment: String?)
+
+    /**
+     * Toggle bookmark
+     * @param key - key of the message to toggle
+     */
     fun toggleMark(key: String)
 
+    /**
+     * Remove messages and search results by a predicate
+     * @param progress - removing progress callback
+     * @param predicate - predicate that asses removing condition
+     * @return Duration of the removal operation
+     */
     suspend fun removeMessages(progress: (Float) -> Unit, predicate: (LogMessage) -> Boolean): Long
 }
