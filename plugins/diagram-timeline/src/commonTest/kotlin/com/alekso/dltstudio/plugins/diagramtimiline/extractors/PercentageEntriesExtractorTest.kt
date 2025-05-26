bf8bed61dcca2792f8941bb7641e3963816ed9c6
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2025-05-26 22:36:19 +0200

    Rewrite Percentage extraction tests

--- plugins/diagram-timeline/src/commonTest/kotlin/com/alekso/dltstudio/plugins/diagramtimiline/extractors/PercentageEntriesExtractorTest.kt
+++ plugins/diagram-timeline/src/commonTest/kotlin/com/alekso/dltstudio/plugins/diagramtimiline/extractors/PercentageEntriesExtractorTest.kt
@@ -2,13 +2,11 @@ package com.alekso.dltstudio.plugins.diagramtimiline.extractors
 
 import com.alekso.dltmessage.SampleData
 import com.alekso.dltstudio.charts.model.PercentageChartData
-import com.alekso.dltstudio.charts.model.PercentageEntry
 import com.alekso.dltstudio.charts.model.StringKey
-import com.alekso.dltstudio.plugins.diagramtimeline.TimeLineEntry
-import com.alekso.dltstudio.plugins.diagramtimeline.TimeLineFloatEntry
 import com.alekso.dltstudio.plugins.diagramtimeline.filters.extractors.EntriesExtractor
 import com.alekso.dltstudio.plugins.diagramtimeline.filters.extractors.PercentageEntriesExtractor
 import org.junit.Test
+import kotlin.test.DefaultAsserter.assertTrue
 import kotlin.test.assertEquals
 
 class PercentageEntriesExtractorTest {
@@ -20,22 +18,21 @@ class PercentageEntriesExtractorTest {
         val dltMessage = SampleData.create(
             timeStampUs = 1234567890L, payloadText = "12% 67% 89%"
         )
-        val pattern = """(?<g1>\d+)%\s(?<g2>\d+)%\s(?<g3>\d+)%"""
+        val key1 = StringKey("g1")
+        val key2 = StringKey("g2")
+        val key3 = StringKey("g3")
 
-        val chartData = PercentageChartData()
-        chartData.addEntry(StringKey("g1"), PercentageEntry(1234567890L, 12f, null))
-        chartData.addEntry(StringKey("g2"), PercentageEntry(1234567890L, 67f, null))
-        chartData.addEntry(StringKey("g3"), PercentageEntry(1234567890L, 89f, null))
-        val expected = chartData.getKeys().toSet()
-
-        val actual = PercentageChartData()
+        val actualChartData = PercentageChartData()
         extractor.extractEntry(
             dltMessage,
-            pattern.toRegex(),
+            """(?<g1>\d+)%\s(?<g2>\d+)%\s(?<g3>\d+)%""".toRegex(),
             EntriesExtractor.ExtractionType.NamedGroupsManyEntries,
-            actual
+            actualChartData
         )
-        assertEquals(expected, actual.getKeys().toSet())
+        assertEquals(setOf(key1, key2, key3), actualChartData.getKeys().toSet())
+        assertTrue("", actualChartData.getEntries(key1)[0].value == 12f)
+        assertTrue("", actualChartData.getEntries(key2)[0].value == 67f)
+        assertTrue("", actualChartData.getEntries(key3)[0].value == 89f)
     }
 
     @Test
@@ -43,19 +40,17 @@ class PercentageEntriesExtractorTest {
         val dltMessage = SampleData.create(
             timeStampUs = 1234567890L, payloadText = "GPU Load: 5.18%, Preemptions: 39"
         )
-        val pattern = """GPU Load:\s+(?<value>\d+.\d+)%(?<key>)"""
+        val key = StringKey("")
 
-        val expected = listOf<TimeLineEntry<*>>(
-            TimeLineFloatEntry(1234567890L, "", 5.18f),
-        ).toSet()
-
-        val actual = extractor.extractEntry(
+        val actualChartData = PercentageChartData()
+        extractor.extractEntry(
             dltMessage,
-            pattern.toRegex(),
+            """GPU Load:\s+(?<value>\d+.\d+)%(?<key>)""".toRegex(),
             EntriesExtractor.ExtractionType.NamedGroupsOneEntry,
-            (entries as PercentageChartData)
-        ).toSet()
-        assertEquals(expected, actual)
+            actualChartData
+        )
+        assertEquals(setOf(key), actualChartData.getKeys().toSet())
+        assertTrue("", actualChartData.getEntries(key)[0].value == 5.18f)
     }
 
     @Test
@@ -63,21 +58,21 @@ class PercentageEntriesExtractorTest {
         val dltMessage = SampleData.create(
             timeStampUs = 1234567890L, payloadText = "cpu0: 12% cpu1: 34% cpu3: 66%"
         )
-        val pattern = """(.*):\s(\d+)%\s(.*):\s(\d+)%\s(.*):\s(\d+)%"""
+        val key1 = StringKey("cpu0")
+        val key2 = StringKey("cpu1")
+        val key3 = StringKey("cpu3")
 
-        val expected = listOf<TimeLineEntry<*>>(
-            TimeLineFloatEntry(1234567890L, "cpu0", 12f),
-            TimeLineFloatEntry(1234567890L, "cpu1", 34f),
-            TimeLineFloatEntry(1234567890L, "cpu3", 66f),
-        ).toSet()
-
-        val actual = extractor.extractEntry(
+        val actualChartData = PercentageChartData()
+        extractor.extractEntry(
             dltMessage,
-            pattern.toRegex(),
+            """(.*):\s(\d+)%\s(.*):\s(\d+)%\s(.*):\s(\d+)%""".toRegex(),
             EntriesExtractor.ExtractionType.GroupsManyEntries,
-            (entries as PercentageChartData)
-        ).toSet()
-        assertEquals(expected, actual)
+            actualChartData
+        )
+        assertEquals(setOf(key1, key2, key3), actualChartData.getKeys().toSet())
+        assertTrue("", actualChartData.getEntries(key1)[0].value == 12f)
+        assertTrue("", actualChartData.getEntries(key2)[0].value == 34f)
+        assertTrue("", actualChartData.getEntries(key3)[0].value == 66f)
     }
 
 }
