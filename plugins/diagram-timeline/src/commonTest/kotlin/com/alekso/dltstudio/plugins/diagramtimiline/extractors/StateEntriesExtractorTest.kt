e4cd74e71d6f8e92767593291ceb4f8a918976c2
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2025-07-08 19:08:26 +0200

    Fix tests

--- plugins/diagram-timeline/src/commonTest/kotlin/com/alekso/dltstudio/plugins/diagramtimiline/extractors/StateEntriesExtractorTest.kt
+++ plugins/diagram-timeline/src/commonTest/kotlin/com/alekso/dltstudio/plugins/diagramtimiline/extractors/StateEntriesExtractorTest.kt
@@ -3,6 +3,7 @@ package com.alekso.dltstudio.plugins.diagramtimiline.extractors
 import com.alekso.dltmessage.SampleData
 import com.alekso.dltstudio.charts.model.StateChartData
 import com.alekso.dltstudio.charts.model.StringKey
+import com.alekso.dltstudio.model.contract.LogMessage
 import com.alekso.dltstudio.plugins.diagramtimeline.filters.extractors.EntriesExtractor
 import com.alekso.dltstudio.plugins.diagramtimeline.filters.extractors.StateEntriesExtractor
 import org.junit.Test
@@ -15,15 +16,18 @@ class StateEntriesExtractorTest {
 
     @Test
     fun `Test StateEntriesExtractor using named groups`() {
-        val dltMessage = SampleData.create(
-            timeStampUs = 1234567890L, payloadText = "User 10 state changed from LOADING to RUNNING"
+        val logMessage = LogMessage(
+            SampleData.create(
+                timeStampUs = 1234567890L,
+                payloadText = "User 10 state changed from LOADING to RUNNING"
+            )
         )
 
         val key1 = StringKey("10")
 
-        val actualChartData = StateChartData()
+        val actualChartData = StateChartData<LogMessage>()
         extractor.extractEntry(
-            dltMessage,
+            logMessage,
             """User\s(?<key>\d+)\sstate changed from (?<oldvalue>.*) to (?<value>.*)""".toRegex(),
             EntriesExtractor.ExtractionType.NamedGroupsOneEntry,
             actualChartData
@@ -35,15 +39,17 @@ class StateEntriesExtractorTest {
 
     @Test
     fun `Test StateEntriesExtractor with dynamic group names`() {
-        val dltMessage = SampleData.create(
+        val logMessage = LogMessage(
+            SampleData.create(
             timeStampUs = 1234567890L, payloadText = "User 10 state changed from LOADING to RUNNING"
+            )
         )
 
         val key1 = StringKey("10")
 
-        val actualChartData = StateChartData()
+        val actualChartData = StateChartData<LogMessage>()
         extractor.extractEntry(
-            dltMessage,
+            logMessage,
             """User\s(\d+)\sstate changed from (.*) to (.*)""".toRegex(),
             EntriesExtractor.ExtractionType.GroupsManyEntries,
             actualChartData
