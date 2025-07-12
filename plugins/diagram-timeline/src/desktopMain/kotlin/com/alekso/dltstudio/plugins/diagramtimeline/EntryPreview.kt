08485f13620fbd0998c966883d1ebfb18bc503b9
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2025-07-12 21:13:17 +0200

    Entry preview

--- plugins/diagram-timeline/src/desktopMain/kotlin/com/alekso/dltstudio/plugins/diagramtimeline/EntryPreview.kt
+++ plugins/diagram-timeline/src/desktopMain/kotlin/com/alekso/dltstudio/plugins/diagramtimeline/EntryPreview.kt
@@ -1,59 +1,72 @@
 package com.alekso.dltstudio.plugins.diagramtimeline
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.alekso.dltmessage.SampleData
 import com.alekso.dltstudio.charts.model.ChartEntry
-import com.alekso.dltstudio.charts.model.DurationEntry
 import com.alekso.dltstudio.charts.model.EventEntry
-import com.alekso.dltstudio.charts.model.MinMaxEntry
-import com.alekso.dltstudio.charts.model.PercentageEntry
-import com.alekso.dltstudio.charts.model.SingleStateEntry
-import com.alekso.dltstudio.charts.model.StateEntry
 import com.alekso.dltstudio.model.contract.LogMessage
 
 @Composable
 fun EntryPreview(selectedEntry: ChartEntry<LogMessage>?) {
+    val padding = Modifier.padding(4.dp)
     val logMessage = selectedEntry?.data
-    if (logMessage != null) {
-        Column {
-            when (selectedEntry) {
-                is EventEntry -> {
-                    Text("${selectedEntry.event}")
-                }
-
-                is MinMaxEntry -> {
-                    Text("${selectedEntry.value}")
-                }
-
-                is PercentageEntry -> {
-                    Text("${selectedEntry.value}")
-                }
-
-                is DurationEntry -> {
-                    Text("${selectedEntry.begin} -> ${selectedEntry.end}")
-                }
-
-                is SingleStateEntry -> {
-                    Text("${selectedEntry.state}")
-                }
-
-                is StateEntry -> {
-                    Text("${selectedEntry.oldState} -> ${selectedEntry.newState}")
+    if (selectedEntry != null) {
+        val scrollState = rememberScrollState()
+        Box(Modifier.fillMaxSize().background(Color.White)) {
+            SelectionContainer {
+                Column(Modifier.verticalScroll(scrollState).fillMaxSize()) {
+                    Text(text = "Value: ${selectedEntry.getText()}", modifier = padding)
+                    val dltMessage = logMessage?.dltMessage
+                    if (dltMessage != null) {
+                        val timeText = LocalFormatter.current.formatDateTime(dltMessage.timeStampUs)
+                        Text(
+                            text = "#${logMessage.id} $timeText ${logMessage.getMessageText()}",
+                            modifier = padding
+                        )
+                    }
                 }
             }
-            Text("${logMessage.dltMessage.payloadText()}")
+            VerticalScrollbar(
+                modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(),
+                adapter = rememberScrollbarAdapter(
+                    scrollState = scrollState
+                )
+            )
         }
     } else {
-        Text("No event selected")
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text("No event selected")
+            }
+        }
     }
 }
 
 @Preview
 @Composable
 fun PreviewEntryPreview() {
+    val dltMessage = SampleData.create(123L, payloadText = "Test message content")
     Column {
-        EntryPreview(EventEntry(System.currentTimeMillis(), "test"))
+        EntryPreview(EventEntry<LogMessage>(23423423L, "test", LogMessage(dltMessage)))
     }
 }
