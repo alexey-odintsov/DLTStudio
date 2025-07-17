3a0d4f4c626912395a16cbe810dd29d5d51b5c4f
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2025-07-17 22:50:28 +0200

    DesktopDialogWindow added

---
+++ ui-components/src/desktopMain/kotlin/com/alekso/dltstudio/uicomponents/dialogs/DesktopDialogWindow.kt
@@ -0,0 +1,31 @@
+package com.alekso.dltstudio.uicomponents.dialogs
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.window.DialogState
+import androidx.compose.ui.window.DialogWindow
+import androidx.compose.ui.window.DialogWindowScope
+import androidx.compose.ui.window.rememberDialogState
+import com.alekso.dltstudio.theme.ThemeManager
+
+@Composable
+fun DesktopDialogWindow(
+    onCloseRequest: () -> Unit,
+    state: DialogState = rememberDialogState(),
+    visible: Boolean = true,
+    title: String = "Untitled",
+    icon: Painter? = null,
+    content: @Composable DialogWindowScope.() -> Unit
+) {
+    DialogWindow(
+        visible = visible,
+        onCloseRequest = onCloseRequest,
+        icon = icon,
+        title = title,
+        state = state,
+    ) {
+        ThemeManager.AppTheme {
+            content()
+        }
+    }
+}
