6ce710957335e7c27c8875e369dd3428261d181b
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2026-02-20 22:55:52 +0100

    release 1.3.0

--- desktop-app/build.gradle.kts
+++ desktop-app/build.gradle.kts
@@ -68,7 +68,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb, TargetFormat.Exe)
             packageName = "alexey.odintsov.dltstudio"
-            packageVersion = "1.2.1"
+            packageVersion = "1.3.0"
         }
     }
     composeCompiler {
