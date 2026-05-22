c979e7fe0af9668032e7261b5d12c9be6a04aa12
Alexey Odintsov <odintsov.aleksey.net@gmail.com>
2026-05-22 20:58:10 +0200

    Fix tests

--- plugins/diagram-timeline/src/commonTest/kotlin/alexey/odintsov/dltstudio/plugins/diagramtimeline/extractors/DurationEntriesExtractorTest.kt
+++ plugins/diagram-timeline/src/commonTest/kotlin/alexey/odintsov/dltstudio/plugins/diagramtimeline/extractors/DurationEntriesExtractorTest.kt
@@ -1,8 +1,8 @@
 package alexey.odintsov.dltstudio.plugins.diagramtimeline.extractors
 
+import alexey.odintsov.charts.model.DurationChartData
+import alexey.odintsov.charts.model.StringKey
 import alexey.odintsov.dltmessage.SampleData
-import alexey.odintsov.dltstudio.charts.model.DurationChartData
-import alexey.odintsov.dltstudio.charts.model.StringKey
 import alexey.odintsov.dltstudio.model.contract.LogMessage
 import alexey.odintsov.dltstudio.plugins.diagramtimeline.filters.extractors.DurationEntriesExtractor
 import alexey.odintsov.dltstudio.plugins.diagramtimeline.filters.extractors.EntriesExtractor
--- plugins/diagram-timeline/src/commonTest/kotlin/alexey/odintsov/dltstudio/plugins/diagramtimeline/extractors/SingleStateEntriesExtractorTest.kt
+++ plugins/diagram-timeline/src/commonTest/kotlin/alexey/odintsov/dltstudio/plugins/diagramtimeline/extractors/SingleStateEntriesExtractorTest.kt
@@ -1,8 +1,8 @@
 package alexey.odintsov.dltstudio.plugins.diagramtimeline.extractors
 
+import alexey.odintsov.charts.model.SingleStateChartData
+import alexey.odintsov.charts.model.StringKey
 import alexey.odintsov.dltmessage.SampleData
-import alexey.odintsov.dltstudio.charts.model.SingleStateChartData
-import alexey.odintsov.dltstudio.charts.model.StringKey
 import alexey.odintsov.dltstudio.model.contract.LogMessage
 import alexey.odintsov.dltstudio.plugins.diagramtimeline.filters.extractors.EntriesExtractor
 import alexey.odintsov.dltstudio.plugins.diagramtimeline.filters.extractors.SingleStateEntriesExtractor
